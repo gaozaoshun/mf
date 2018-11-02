443d062783ff67a90bc07d3fbaa5a26a0a2485fd
gzs <739178184@qq.com>
2018-11-02 17:46:52 +0800

    [新增]获取当前地理位置名称50%

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,65 +9,91 @@
 </template>
 
 <script>
-import card from "@/components/card"
-import { login } from '@/api/login'
+import card from "@/components/card";
+import { login } from "@/api/login";
 
 export default {
   data() {
-    return {
-    }
+    return {};
   },
   components: {
     card
   },
   computed: {
     userInfo() {
-      return this.$store.state.userInfo
+      return this.$store.state.userInfo;
     }
   },
   methods: {
+    // 登录获取用户信息、Token
     getUserInfo(e) {
       if (!e.target.rawData) {
-        console.log("拒绝授权")
-        return
+        console.log("拒绝授权");
+        return;
       }
-      console.log("同意授权")
+      console.log("同意授权");
       // 获取code
       wx.login({
         success: res => {
           // 此时微信端已生成session_key
-          console.log('获临时凭证code', res.code)
-          let code = res.code
+          console.log("获临时凭证code", res.code);
+          let code = res.code;
           wx.getUserInfo({
             withCredentials: true,
             success: res => {
               // 登录请求参数
               let body = {
-                rawData: res.rawData || '',
-                signature: res.signature || '',
-                encryptedData: res.encryptedData || '',
-                iv: res.iv || '',
-                code: code || ''
-              }
+                rawData: res.rawData || "",
+                signature: res.signature || "",
+                encryptedData: res.encryptedData || "",
+                iv: res.iv || "",
+                code: code || ""
+              };
               login(body).then(res => {
                 if (res.code === 100) {
                   // 保存userInfo
-                  this.$store.dispatch('setUserInfo', res.data)
+                  this.$store.dispatch("setUserInfo", res.data);
                   // 保存Token
-                  wx.setStorageSync('token', res.data.token || '')
+                  wx.setStorageSync("token", res.data.token || "");
                 } else {
-                  wx.showToast({ title: '登录失败', icon: 'none' })
+                  wx.showToast({ title: "登录失败", icon: "none" });
                 }
               })
             }
           })
         }
       })
+    },
 
+    // 获取当前地理位置
+    getLocation() {
+      wx.getSetting({
+        success: res => {
+          if (!res.authSetting['scope.userLocation']) {
+            wx.getLocation({
+              type: 'wgs84',
+              success: res => {
+                const latitude = res.latitude
+                const longitude = res.longitude
+              },
+              fail: () => {
+                wx.showToast({
+                  title: '获取当前位置失败',
+                  icon: 'none'
+                })
+              }
+            })
+          } else {
+           
+          }
+        }
+      })
     }
   },
+  created() {
+    // 1.加载当前位置
 
-  created() { }
+  }
 };
 </script>
 
--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -3,10 +3,3 @@ import App from './index'
 
 const app = new Vue(App)
 app.$mount()
-
-export default {
-    config: {
-        navigationBarTitleText: '微信读书书城',
-        enableReachBottom: true
-    }
-}
